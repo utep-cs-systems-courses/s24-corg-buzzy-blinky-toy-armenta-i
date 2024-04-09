70fef797cfba5042d4f62b7e2c10de35a06ab8fb
armenta-i <i_armenta@outlook.com>
2024-04-09 13:35:45 -0600

    first attempt to fix secondUpdate

--- demos/6-led-assy/secondUpdate-s.s
+++ demos/6-led-assy/secondUpdate-s.s
@@ -8,18 +8,19 @@
 		;use extern to declare variables declared in other files 
 
 	.global secondUpdate
+	.extern @secondCount
 	.extern P1OUT
 
 secondUpdate:
-	mov #0, &secondCount
 	add #1, &secondCount	;secondCount++
 	cmp #250, &secondCount	;secondCount >= 250
-	jge reset
-	jmp end
+	jge reset		;if secondCount >= 250
+
+	jmp end			;if secondCount < 250
 	
 reset:
 	mov #0, &secondCount    ;secondCount = 0
 	call oncePerSecond      ;oncePerSecond function call
-
+        jmp end
 end:
-	ret
+	pop r0
